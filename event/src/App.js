c1e95d69c758d30007b6a92876ad1af3d292e443
Krishna SP <152942298+Krishnasp2803@users.noreply.github.com>
2025-01-07 14:56:47 +0000

    app.js edit

--- event/src/App.js
+++ event/src/App.js
@@ -5,6 +5,7 @@ import{BrowserRouter as Router,Route,Routes} from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Calendar from './pages/Calendar';
+import UserProfile from './pages/UserProfile';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path='/adminhome'element={<Home/>}></Route>
         <Route path='/aevent'element={<Allevents/>}></Route>
         <Route path='/acalendar'element={<Calendar/>}></Route>
+        <Route path="/userprofile" element={<UserProfile />} />
         </Routes>
       </Router>
     </div>
